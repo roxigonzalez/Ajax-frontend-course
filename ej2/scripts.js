a7a4cf2524ebd976c023f1affdf870e504df4362
Roxana G. Espinal <roxana.gonzalez21@gmail.com>
2019-05-29 17:51:16 -0600

    Mas bonito!

--- ej2/scripts.js
+++ ej2/scripts.js
@@ -1,32 +1,42 @@
-$("#boton-usuarios").on("click", getUsers);
-
-function getUsers() {
-  $.ajax({
-    url: "https://reqres.in/api/users",
-    success: function(respuesta) {
-      var listaUsuarios = $("#lista-usuarios");
-      for (var i = 0; i < respuesta.data.length; i++) {
-        var elemento = respuesta.data[i];
-        console.log(elemento);
-        listaUsuarios.append(
-          "<div>" +
-            "<p>" +
-            elemento.first_name +
-            " " +
-            elemento.last_name +
-            "</p>" +
-            "<img src=" +
-            elemento.avatar +
-            "></img>" +
-            "</div>"
-        );
-      }
-      // $.each(respuesta.data, function(index, elemento) {
-      //
-      // });
-    },
-    error: function() {
-      console.log("No se ha podido obtener la información");
-    }
+$(document).ready(function() {
+  $("#btn-get-users").click(function() {
+    // console.log("hice click!");
+    getUsers();
   });
-}
+
+  function getUsers() {
+    $.ajax({
+      url: "https://reqres.in/api/users?per_page=12",
+      type: "GET",
+      dataType: "json",
+      success: function(respuesta) {
+        var divListaUsuarios = $("#lista-usuarios");
+        console.log(respuesta.data);
+        var collectionUsers = respuesta.data;
+        var newElement = "";
+        for (var i = 0; i < collectionUsers.length; i++) {
+          var user = collectionUsers[i];
+          newElement +=
+            "<div><span>" +
+            user.first_name +
+            " " +
+            user.last_name +
+            "<br/></span>" +
+            "<strong>Correo: " +
+            user.email +
+            "</strong><br/>" +
+            "<img src=" +
+            user.avatar +
+            " /></hr></div>";
+        }
+        divListaUsuarios.html(newElement);
+      },
+      error: function(error) {
+        alert("Ha ocurrido un error");
+      },
+      complete: function(status) {
+        // alert("petición finalizada!");
+      }
+    });
+  }
+});
